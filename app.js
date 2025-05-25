2eb8038b350662d3d4f14410c049bc63decd1126
Hadixp <hadi.b.2002@gmail.com>
2025-05-25 10:30:57 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// this fuction checks the users device and redirect it to mobile or desktop view page
 function redirect(){
+    // getting user browser data and device
     const userData = navigator.userAgent;
     if(userData.includes('Android' || 'Iphone')){
         window.location.href = './Mobile-view/Mobile.html'
@@ -6,4 +8,5 @@ function redirect(){
         window.location.href = './Desktop-view/Desktop.html'
     }
 }
+// event listeneter on load page
 window.addEventListener('load',redirect);
